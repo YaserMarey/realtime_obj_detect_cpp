f325588af89766fe93e918e72a94e92f67228595
Yaser <ymarey3@gatech.edu>
2020-09-17 21:00:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Yolo in C++ applied to web cam feed for realtime object detection
+#### download model from:
+https://pjreddie.com/media/files/yolov3.weights
+
+https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
---
+++ realtime_obj_detect_cpp.cpp
@@ -0,0 +1,238 @@
+#include <fstream>
+#include <sstream>
+
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+float confThreshold, nmsThreshold;
+vector<string> classes;
+
+inline void preprocess(const Mat& frame, Net& net, Size inpSize, float scale,
+    const Scalar& mean, bool swapRB);
+
+void postprocess(Mat& frame, const std::vector<Mat>& out, Net& net);
+
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
+
+int main(int argc, char** argv)
+{
+    confThreshold = 0.4;
+    nmsThreshold = 0.2;
+    float scale = 1.0;
+    Scalar mean = Scalar(0, 0, 0);
+    bool swapRB = true;
+    int inpWidth = 416;
+    int inpHeight = 416;
+    size_t asyncNumReq = 0;
+    string modelPath = "yolov3.weights";
+    string configPath = "yolov3.cfg";
+
+    ifstream ifs("coco.names");
+    if (!ifs.is_open())
+        CV_Error(Error::StsError, "File coco.names not found");
+    string line;
+    while (getline(ifs, line))
+    {
+        classes.push_back(line);
+    }
+
+    // Load a model.
+    Net net = readNet("yolov3.weights", "yolov3.cfg");
+
+    vector<String> outNames = net.getUnconnectedOutLayersNames();
+
+    // Create a window
+    namedWindow("", WINDOW_NORMAL);
+
+    // Open a video file or an image file or a camera stream.
+    VideoCapture cap;
+    //cap.open("traffic1.mp4");
+    cap.open(1);
+
+    // Process frames.
+    Mat frame, blob;
+    while (waitKey(1) < 0)
+    {
+        cap >> frame;
+        if (frame.empty())
+        {
+            waitKey();
+            break;
+        }
+
+        preprocess(frame, net, Size(416, 416),
+            1.0, Scalar(0, 0, 0), true);
+
+        vector<Mat> outs;
+        net.forward(outs, outNames);
+
+        postprocess(frame, outs, net);
+
+        imshow("", frame);
+
+        //if (waitKey(1) < 0)
+        //    break;
+
+    }
+    return 0;
+}
+
+inline void preprocess(const Mat& frame, Net& net, Size inpSize, float scale,
+    const Scalar& mean, bool swapRB)
+{
+    static Mat blob;
+    blobFromImage(frame, blob, 1.0, inpSize, Scalar(), swapRB, false, CV_8U);
+
+    // Run a model.
+    net.setInput(blob, "", scale, mean);
+}
+
+void postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net)
+{
+    static vector<int> outLayers = net.getUnconnectedOutLayers();
+    static string outLayerType = net.getLayer(outLayers[0])->type;
+
+    vector<int> classIds;
+    vector<float> confidences;
+    vector<Rect> boxes;
+    if (outLayerType == "DetectionOutput")
+    {
+        // Network produces output blob with a shape 1x1xNx7 where N is a number of
+        // detections and an every detection is a vector of values
+        // [batchId, classId, confidence, left, top, right, bottom]
+        CV_Assert(outs.size() > 0);
+        for (size_t k = 0; k < outs.size(); k++)
+        {
+            float* data = (float*)outs[k].data;
+            for (size_t i = 0; i < outs[k].total(); i += 7)
+            {
+                float confidence = data[i + 2];
+                if (confidence > confThreshold)
+                {
+                    int left = (int)data[i + 3];
+                    int top = (int)data[i + 4];
+                    int right = (int)data[i + 5];
+                    int bottom = (int)data[i + 6];
+                    int width = right - left + 1;
+                    int height = bottom - top + 1;
+                    if (width <= 2 || height <= 2)
+                    {
+                        left = (int)(data[i + 3] * frame.cols);
+                        top = (int)(data[i + 4] * frame.rows);
+                        right = (int)(data[i + 5] * frame.cols);
+                        bottom = (int)(data[i + 6] * frame.rows);
+                        width = right - left + 1;
+                        height = bottom - top + 1;
+                    }
+                    classIds.push_back((int)(data[i + 1]) - 1);  // Skip 0th background class id.
+                    boxes.push_back(Rect(left, top, width, height));
+                    confidences.push_back(confidence);
+                }
+            }
+        }
+    }
+    else if (outLayerType == "Region")
+    {
+        for (size_t i = 0; i < outs.size(); ++i)
+        {
+            // Network produces output blob with a shape NxC where N is a number of
+            // detected objects and C is a number of classes + 4 where the first 4
+            // numbers are [center_x, center_y, width, height]
+            float* data = (float*)outs[i].data;
+            for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
+            {
+                Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
+                Point classIdPoint;
+                double confidence;
+                minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
+                if (confidence > confThreshold)
+                {
+                    int centerX = (int)(data[0] * frame.cols);
+                    int centerY = (int)(data[1] * frame.rows);
+                    int width = (int)(data[2] * frame.cols);
+                    int height = (int)(data[3] * frame.rows);
+                    int left = centerX - width / 2;
+                    int top = centerY - height / 2;
+
+                    classIds.push_back(classIdPoint.x);
+                    confidences.push_back((float)confidence);
+                    boxes.push_back(Rect(left, top, width, height));
+                }
+            }
+        }
+    }
+    else
+        CV_Error(Error::StsNotImplemented, "Unknown output layer type: " + outLayerType);
+
+    // NMS is used inside Region layer only on DNN_BACKEND_OPENCV for another backends we need NMS in sample
+    // or NMS is required if number of outputs > 1
+    if (outLayers.size() > 1 || (outLayerType == "Region"))
+    {
+        std::map<int, std::vector<size_t> > class2indices;
+        for (size_t i = 0; i < classIds.size(); i++)
+        {
+            if (confidences[i] >= confThreshold)
+            {
+                class2indices[classIds[i]].push_back(i);
+            }
+        }
+        std::vector<Rect> nmsBoxes;
+        std::vector<float> nmsConfidences;
+        std::vector<int> nmsClassIds;
+        for (std::map<int, std::vector<size_t> >::iterator it = class2indices.begin(); it != class2indices.end(); ++it)
+        {
+            std::vector<Rect> localBoxes;
+            std::vector<float> localConfidences;
+            std::vector<size_t> classIndices = it->second;
+            for (size_t i = 0; i < classIndices.size(); i++)
+            {
+                localBoxes.push_back(boxes[classIndices[i]]);
+                localConfidences.push_back(confidences[classIndices[i]]);
+            }
+            std::vector<int> nmsIndices;
+            NMSBoxes(localBoxes, localConfidences, confThreshold, nmsThreshold, nmsIndices);
+            for (size_t i = 0; i < nmsIndices.size(); i++)
+            {
+                size_t idx = nmsIndices[i];
+                nmsBoxes.push_back(localBoxes[idx]);
+                nmsConfidences.push_back(localConfidences[idx]);
+                nmsClassIds.push_back(it->first);
+            }
+        }
+        boxes = nmsBoxes;
+        classIds = nmsClassIds;
+        confidences = nmsConfidences;
+    }
+
+    for (size_t idx = 0; idx < boxes.size(); ++idx)
+    {
+        Rect box = boxes[idx];
+        drawPred(classIds[idx], confidences[idx], box.x, box.y,
+            box.x + box.width, box.y + box.height, frame);
+    }
+}
+
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
+{
+    rectangle(frame, Point(left, top), Point(right, bottom), Scalar(0, 255, 0));
+
+    string label = format("%.2f", conf);
+    if (!classes.empty())
+    {
+        CV_Assert(classId < (int)classes.size());
+        label = classes[classId] + ": " + label;
+    }
+
+    int baseLine;
+    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+
+    top = max(top, labelSize.height);
+    rectangle(frame, Point(left, top - labelSize.height),
+        Point(left + labelSize.width, top + baseLine), Scalar::all(255), FILLED);
+    putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.5, Scalar());
+}
+
